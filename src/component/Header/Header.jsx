f979cfdef0479238be587c2ca49061e32f033c2d
FionaNatasha <limanskoe1-3@mail.ru>
2022-09-05 22:10:51 +0300

    burger_menu

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,19 +1,21 @@
 import { NavLink } from "react-router-dom";
 import "./header.css";
 import NavLinkList from "./NavLinkList";
+import {useState} from 'react'
 
 function Header(props) {
+  let [openMenu, isOpenMenu] = useState(false)
   return (
     <header className="header">
       <div className="container">
         <div className="logo">
           <NavLink to="/">AitishNik</NavLink>
         </div>
-        <div className="nav_link">
+        <div className={ openMenu?"nav_link_open":"nav_link"}>
           <NavLinkList {...props} />
         </div>
         <div className="menu_burger">
-          <span></span>
+          <span onClick={()=>isOpenMenu(!openMenu)}></span> 
         </div>
       </div>
     </header>
